a532686e1c10228d81a935c202527300af1af316
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2023-06-09 16:32:01 +0700

    change config

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,7 +9,7 @@
 
 describe('Login spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/login');
+    cy.visit('http://127.0.0.1:5173/login');
   });
 
   it('should display login page correctly', () => {
